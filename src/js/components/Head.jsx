87758ba31e6c3be2a13796aa5694baaf2ada4248
Egor <meekgot@gmail.com>
2020-07-21 19:08:54 +0300

    fix - fixed exp block styles

--- src/js/components/Head.jsx
+++ src/js/components/Head.jsx
@@ -4,7 +4,6 @@ import { Helmet } from 'react-helmet';
 export default function Head({ title }) {
     return (
         <Helmet>
-            <meta charSet="utf-8" />
             <title>{ title }</title>
         </Helmet>
     )
